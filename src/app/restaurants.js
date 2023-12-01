0edcd5fd71dc0830391e723400198d91027f333c
Pat McCue <psmccue@gmail.com>
2023-11-30 21:43:09 -0700

    Beginning restaurant app

--- src/app/restaurants.js
+++ src/app/restaurants.js
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Restaurants() {
   return (
-    <h1>Hello World</h1>
+    <h1>Restaurants Page</h1>
   ) 
 }
